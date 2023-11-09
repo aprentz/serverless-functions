884e7b900d676c16a5a029841505d30daf184569
AlexEatsBurgers <in.the_eye@hotmail.com>
2023-11-09 20:08:52 +1100

    mailchimp testing

--- examples/6-newsletter/app.js
+++ examples/6-newsletter/app.js
@@ -6,14 +6,13 @@ alert.style.display = 'none'
 
 form.addEventListener('submit', async (e) => {
    e.preventDefault()
-   form.classList.add('loading')
+   // form.classList.add('loading')
    alert.style.display = 'none'
    const email = emailInput.value
    try {
       await axios.post('/api/6-newsletter', { email })
-      form.innerHTML = 'h<4>'
    } catch (error) {
-
+      console.log(error)
    }
 })
 
--- functions/6-newsletter.js
+++ functions/6-newsletter.js
@@ -1,31 +1,35 @@
-
 const mailchimp = require("@mailchimp/mailchimp_marketing");
 const client = require("@mailchimp/mailchimp_marketing");
 
 
 exports.handler = async (event, context) => {
+   const method = event.httpMethod
+   const { email } = JSON.parse(event.body)
+   console.log(email)
    try {
       mailchimp.setConfig({
          apiKey: process.env.MAILCHIMP_API_KEY,
          server: "us9",
       });
 
-      // const response = await client.lists.getListMembersInfo("f28a67cb73");
+      // const members = await client.lists.getListMembersInfo(process.env.MAILCHIMP_LIST_ID);
       // const {total_items} = response
+      
 
-      const response = await client.lists.addListMember(process.env.MAILCHIMP_LIST_ID, {
-         email_address: "100millionsemails@hotmail.com",
-         status: "pending",
+      const response = await client.lists.addListMember("f28a67cb73", {
+         email_address: email,
+         status: "subscribed",
       });
-
 
       return {
          statusCode: 200,
-         body: JSON.stringify(`Total Items: ${total_items}`)
+         body: JSON.stringify(response)
       }
    }
    catch (error) {
-      console.log(error)
-      return { statusCode: 500, body: 'error alert' }
+      return {
+         statusCode: 500,
+         body: JSON.stringify(error.response.error)
+      }
    }
 }
