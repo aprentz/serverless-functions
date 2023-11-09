0aef4e8920c49ff19846ac02d5d15553d129d825
AlexEatsBurgers <in.the_eye@hotmail.com>
2023-11-09 16:33:46 +1100

    started newsletter section

--- examples/6-newsletter/app.js
+++ examples/6-newsletter/app.js
@@ -2,3 +2,16 @@ const form = document.querySelector('.form')
 const emailInput = document.querySelector('.email-input')
 const alert = document.querySelector('.alert')
 alert.style.display = 'none'
+
+form.addEventListener('submit', async (e) => {
+   e.preventDefault()
+   form.classList.add('loading')
+   alert.style.display = 'none'
+   const email = emailInput.value
+   try {
+      await axios.post('/api/6-newsletter', { email })
+      form.innerHTML = '>h4>'
+   } catch (error) {
+
+   }
+})
