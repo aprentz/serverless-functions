82ae8fc7c8ca69bb1aee6e979a6c249c08c8141a
AlexEatsBurgers <in.the_eye@hotmail.com>
2023-12-02 17:07:30 +1100

    stripe complete

--- examples/8-stripe/app.js
+++ examples/8-stripe/app.js
@@ -1,122 +1,129 @@
 const purchase = [
-  { id: '1', name: 't-shirt', price: 1999 },
-  { id: '2', name: 'shoes', price: 4999 },
-]
-const total_amount = 6998
-const shipping_fee = 1099
-var stripe = Stripe('YOUR_PUBLIC_KEY')
+   { id: '1', name: 't-shirt', price: 1999 },
+   { id: '2', name: 'shoes', price: 4999 },
+ ]
+ const total_amount = 10998
+ const shipping_fee = 1099
+
+
+const stripe = Stripe("pk_test_51OB5jbFlwxELi2xhPooqP3Qre8iuvS07yQT6PCryNrjjZpRtmCQ2kwBF4LEBC7OUoWBEbJFQzxDqnMzrlu5cqxWw00zdySvWqO");
 
 // The items the customer wants to buy
+const items = [{ id: "xl-tshirt", price: "price_CBb6IXqvTLXp3f" }];
 
-// Disable the button until we have Stripe set up on the page
-document.querySelector('button').disabled = true
-fetch('/api/8-stripe', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({ purchase, total_amount, shipping_fee }),
-})
-  .then(function (result) {
-    return result.json()
-  })
-  .then(function (data) {
-    var elements = stripe.elements()
+let elements;
 
-    var style = {
-      base: {
-        color: '#32325d',
-        fontFamily: 'Arial, sans-serif',
-        fontSmoothing: 'antialiased',
-        fontSize: '16px',
-        '::placeholder': {
-          color: '#32325d',
-        },
+initialize();
+checkStatus();
+
+document
+   .querySelector("#payment-form")
+   .addEventListener("submit", handleSubmit);
+
+// Fetches a payment intent and captures the client secret
+async function initialize() {
+   const response = await fetch("/api/8-stripe", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ purchase, total_amount, shipping_fee }),
+   });
+   const { clientSecret } = await response.json();
+
+   const appearance = {
+      theme: 'stripe',
+      variables: {
+        colorPrimary: '#85d996',
+        colorText: '#000000',
       },
-      invalid: {
-        fontFamily: 'Arial, sans-serif',
-        color: '#fa755a',
-        iconColor: '#fa755a',
-      },
-    }
+   };
+   elements = stripe.elements({ appearance, clientSecret });
 
-    var card = elements.create('card', { style: style })
-    // Stripe injects an iframe into the DOM
-    card.mount('#card-element')
+   const paymentElementOptions = {
+      layout: "tabs",
+   };
 
-    card.on('change', function (event) {
-      // Disable the Pay button if there are no card details in the Element
-      document.querySelector('button').disabled = event.empty
-      document.querySelector('#card-error').textContent = event.error
-        ? event.error.message
-        : ''
-    })
-
-    var form = document.getElementById('payment-form')
-    form.addEventListener('submit', function (event) {
-      event.preventDefault()
-      // Complete payment when the submit button is clicked
-      payWithCard(stripe, card, data.clientSecret)
-    })
-  })
-
-// Calls stripe.confirmCardPayment
-// If the card requires authentication Stripe shows a pop-up modal to
-// prompt the user to enter authentication details without leaving your page.
-var payWithCard = function (stripe, card, clientSecret) {
-  loading(true)
-  stripe
-    .confirmCardPayment(clientSecret, {
-      payment_method: {
-        card: card,
-      },
-    })
-    .then(function (result) {
-      if (result.error) {
-        // Show error to your customer
-        showError(result.error.message)
-      } else {
-        // The payment succeeded!
-        orderComplete(result.paymentIntent.id)
-      }
-    })
+   const paymentElement = elements.create("payment", paymentElementOptions);
+   paymentElement.mount("#payment-element");
 }
 
-/* ------- UI helpers ------- */
+async function handleSubmit(e) {
+   e.preventDefault();
+   setLoading(true);
+   const { error } = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+         // Make sure to change this to your payment completion page
+         return_url: "http://localhost:8888/examples/8-stripe/index.html",
+         receipt_email: 'alex.rentz@outlook.com',
+      },
+   });
 
-// Shows a success message when the payment is complete
-var orderComplete = function (paymentIntentId) {
-  loading(false)
-  document
-    .querySelector('.result-message a')
-    .setAttribute(
-      'href',
-      'https://dashboard.stripe.com/test/payments/' + paymentIntentId
-    )
-  document.querySelector('.result-message').classList.remove('hidden')
-  document.querySelector('button').disabled = true
+   // This point will only be reached if there is an immediate error when
+   // confirming the payment. Otherwise, your customer will be redirected to
+   // your `return_url`. For some payment methods like iDEAL, your customer will
+   // be redirected to an intermediate site first to authorize the payment, then
+   // redirected to the `return_url`.
+   if (error.type === "card_error" || error.type === "validation_error") {
+      showMessage(error.message);
+   } else {
+      showMessage("An unexpected error occurred.");
+   }
+
+   setLoading(false);
 }
 
-// Show the customer the error from Stripe if their card fails to charge
-var showError = function (errorMsgText) {
-  loading(false)
-  var errorMsg = document.querySelector('#card-error')
-  errorMsg.textContent = errorMsgText
-  setTimeout(function () {
-    errorMsg.textContent = ''
-  }, 4000)
+// Fetches the payment intent status after payment submission
+async function checkStatus() {
+   const clientSecret = new URLSearchParams(window.location.search).get(
+      "payment_intent_client_secret"
+   );
+
+   if (!clientSecret) {
+      return;
+   }
+
+   const { paymentIntent } = await stripe.retrievePaymentIntent(clientSecret);
+
+   switch (paymentIntent.status) {
+      case "succeeded":
+         showMessage("Payment succeeded!");
+         break;
+      case "processing":
+         showMessage("Your payment is processing.");
+         break;
+      case "requires_payment_method":
+         showMessage("Your payment was not successful, please try again.");
+         break;
+      default:
+         showMessage("Something went wrong.");
+         break;
+   }
+}
+
+// ------- UI helpers -------
+
+function showMessage(messageText) {
+   const messageContainer = document.querySelector("#payment-message");
+
+   messageContainer.classList.remove("hidden");
+   messageContainer.textContent = messageText;
+
+   setTimeout(function () {
+      messageContainer.classList.add("hidden");
+      messageContainer.textContent = "";
+   }, 4000);
 }
 
 // Show a spinner on payment submission
-var loading = function (isLoading) {
-  if (isLoading) {
-    // Disable the button and show a spinner
-    document.querySelector('button').disabled = true
-    document.querySelector('#spinner').classList.remove('hidden')
-    document.querySelector('#button-text').classList.add('hidden')
-  } else {
-    document.querySelector('button').disabled = false
-    document.querySelector('#spinner').classList.add('hidden')
-    document.querySelector('#button-text').classList.remove('hidden')
-  }
+function setLoading(isLoading) {
+   if (isLoading) {
+      // Disable the button and show a spinner
+      document.querySelector("#submit").disabled = true;
+      document.querySelector("#spinner").classList.remove("hidden");
+      document.querySelector("#button-text").classList.add("hidden");
+   } else {
+      document.querySelector("#submit").disabled = false;
+      document.querySelector("#spinner").classList.add("hidden");
+      document.querySelector("#button-text").classList.remove("hidden");
+   }
 }
